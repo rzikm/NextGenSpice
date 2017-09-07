e85678024fa54a11c852db65884e7d6bfb44bd39
Radek Zikmund <r.zikmund.rz@gmail.com>
2017-09-07 23:35:55 +0200

    Moved test code to test project

---
+++ NextGenSpiceTests/Misc/TestBaseQD.cs
@@ -0,0 +1,78 @@
+﻿using Numerics;
+using System;
+using System.Diagnostics;
+
+namespace NextGenSpiceTests.Misc
+{
+    public abstract class TestBaseQD
+    {
+        protected delegate void DllCallback();
+
+        private qd_real[,] matrix;
+        private qd_real[,] matrix2;
+        private long i = 0;
+
+        public TestBaseQD(int size)
+        {
+            matrix2 = new qd_real[size, size + 1];
+            Random rnd = new Random(30);
+            for (var i = 0; i < size; i++)
+            for (var j = 0; j < size; j++)
+            {
+//              Hilbert matrix
+//                matrix2[i, j] = 1 / (qd_real.Zero + i + j + 1);
+//                matrix2[i, size] += matrix2[i, j];
+
+                var val = rnd.NextDouble();
+                matrix2[i, j] += val;
+                matrix2[j, i] += val;
+                matrix2[i, size] += val;
+                matrix2[j, size] += val;
+            }
+        }
+
+        public unsafe qd_real[] RunTest_Native()
+        {
+            matrix = (qd_real[,]) matrix2.Clone();
+            Stopwatch sw = Stopwatch.StartNew();
+            fixed (qd_real* m = matrix)
+            {
+                Call_Native(m, matrix.GetLength(0), Callback);
+            }
+            sw.Stop();
+            Console.WriteLine($"Native:  {sw.Elapsed}");
+            return GetResults();
+        }
+
+        protected abstract unsafe void Call_Native(qd_real* qdReal, int getLength, DllCallback callback);
+
+        protected virtual qd_real[] GetResults()
+        {
+            qd_real[] ret = new qd_real[matrix.GetLength(0)];
+            for (int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = matrix[i, ret.Length];
+            }
+            return ret;
+        }
+
+        public void Callback()
+        {
+            i++;
+        }
+
+        public qd_real[] RunTest_Managed()
+        {
+            matrix = (qd_real[,]) matrix2.Clone();
+            Stopwatch sw = Stopwatch.StartNew();
+            {
+                Call_Managed(matrix, matrix.GetLength(0), Callback);
+            }
+            sw.Stop();
+            Console.WriteLine($"Managed: {sw.Elapsed}");
+            return GetResults();
+        }
+
+        protected abstract void Call_Managed(qd_real[,] qdReals, int getLength, DllCallback callback);
+    }
+}
---
+++ NextGenSpiceTests/Misc/TestGEDouble.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NextGenSpiceTests.Misc
+{
+    public unsafe class TestGeDouble : TestBaseDouble
+    {
+
+        [DllImport("NumericCore.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern void Run_d(double* matrix, int size, DllCallback callback);
+
+        public TestGeDouble(int size) : base(size)
+        {
+        }
+
+        protected override unsafe void Call_Native(double* d, int size, DllCallback callback)
+        {
+            Run_d(d, size, callback);
+        }
+        
+        protected override void Call_Managed(double[,] doubles, int getLength, TestBaseDouble.DllCallback callback)
+        {
+            RunManaged(doubles, getLength, callback);
+        }
+
+        static void RunManaged(double[,] m, int size, DllCallback callback)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                // Search for maximum in this column
+                double maxEl = Math.Abs(m[i, i]);
+                int maxRow = i;
+                for (int k = i + 1; k < size; k++)
+                {
+                    if (Math.Abs(m[k, i]) > maxEl)
+                    {
+                        maxEl = Math.Abs(m[k, i]);
+                        maxRow = k;
+                    }
+                }
+
+                // Swap maximum row with current row (column by column)
+                for (int k = i; k < size + 1; k++)
+                {
+                    double tmp = m[maxRow, k];
+                    m[maxRow, k] = m[i, k];
+                    m[i, k] = tmp;
+                    callback();
+                }
+
+                // Make all rows below this one 0 in current column
+                for (int k = i + 1; k < size; k++)
+                {
+                    double c = -m[k, i] / m[i, i];
+                    for (int j = i; j < size + 1; j++)
+                    {
+                        if (i == j)
+                        {
+                            m[k, j] = 0;
+                        }
+                        else
+                        {
+                            m[k, j] += c * m[i, j];
+                        }
+                    }
+                }
+            }
+
+            // GaussElimSolve equation Ax=b for an upper triangular matrix A
+            for (int i = size - 1; i >= 0; i--)
+            {
+                m[i, size] = m[i, size] / m[i, i];
+                for (int k = i - 1; k >= 0; k--)
+                {
+                    m[k, size] -= m[k, i] * m[i, size];
+                }
+                callback();
+            }
+        }
+    }
+}
